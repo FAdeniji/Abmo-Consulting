22118780fa9fb1f21e1ad1f698b8bd4142fe63dc
Adefolarin Adeniji <adefolarin.adeniji@gmail.com>
2023-05-14 22:32:56 +0100

    WIP

---
+++ nav.php
@@ -0,0 +1,32 @@
+<?php
+    $index = '';
+    $about = '';
+    $counselor = '';
+    $services = '';
+    $pricing = '';
+    $blog = '';
+    $contact = '';
+
+    switch ($pageNumber) {
+        case 1:
+            $index = '';
+          break;
+        case 2:
+          code to be executed if n=label2;
+          break;
+        case 3:
+          code to be executed if n=label3;
+          break;
+        case 4:
+            code to be executed if n=label3;
+            break;
+        case 6:
+            code to be executed if n=label3;
+            break;
+        case 6:
+            code to be executed if n=label3;
+            break;
+        default:
+          $index = '';
+      }
+?>
